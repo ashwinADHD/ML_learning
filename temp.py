36f77ae1b05a8d1c33b162fa852e52e5c25dc7b1
ashwinADHD <86740394+ashwinADHD@users.noreply.github.com>
2021-08-05 22:55:56 -0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -21,10 +21,12 @@ output = np.zeros((8,8))
 B0 = image_array[0:3,0:3]
 k=0
 for i in range(8):
-# B = image_array[0:3,0+i:3+i]
-for j in range(8):
-C = image[0+j:3+j,0+i:3+i]
-piece_wise_multi = np.multiply(C,Kernel)
+    B = image_array[0:3,0+i:3+i]
+    for j in range(8):
+            C = image_array[0+j:3+j,0+i:3+i]
+            piece_wise_multi = np.multiply(C,Kernel)
 # while k < len(z):
 output[i,j]=np.sum(piece_wise_multi)
+
+print(output)
 
